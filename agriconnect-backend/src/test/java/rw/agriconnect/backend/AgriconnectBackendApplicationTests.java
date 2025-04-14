53f1f7086459444c22138eb76fdaecf54fbc50a6
Livia-11 <kirezilivia@gmail.com>
2025-04-14 11:24:08 +0200

    first commit

---
+++ agriconnect-backend/src/test/java/rw/agriconnect/backend/AgriconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.agriconnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
